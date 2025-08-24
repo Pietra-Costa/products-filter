d1fceb17c0485848940e0d1e8ddf28ab1afd21fc
Pietra <pietracosta.dev@gmail.com>
2025-08-24 20:38:30 -0300

    criar objetos

---
+++ script.js
@@ -0,0 +1,45 @@
+const inputValue = document.querySelector("#input")
+const searchBtn = document.querySelector("#search")
+
+const produtos = [
+    {
+        nome: "Jaqueta de couro preta",
+        categoria: "Jacket",
+        image: "assets/black-leather-jacket.jpg"
+    },
+     {
+        nome: "Jaqueta Marrom",
+        categoria: "Jacket",
+        image: "assets/brown-jacket.jpg"
+    },
+     {
+        nome: "Calça moletom cinza",
+        categoria: "Bottom",
+        image: "assets/comfy-gray-pants.jpg"
+    },
+     {
+        nome: "Cropped lã",
+        categoria: "Top",
+        image: "assets/knitted-top.jpg"
+    },
+     {
+        nome: "calça rosa",
+        categoria: "Bottom",
+        image: "assets/pink-trousers.jpg"
+    },
+     {
+        nome: "Saia",
+        categoria: "Bottom",
+        image: "assets/short-skirt.jpg"
+    },
+     {
+        nome: "SmartWatch",
+        categoria: "Acessories",
+        image: "assets/sporty-smartwatch.jpg"
+    },
+     {
+        nome: "Camiseta branca",
+        categoria: "Top",
+        image: "assets/white-tshirt.jpg"
+    }
+]
